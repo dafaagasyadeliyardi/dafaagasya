3b13ae1d5ad772b37f1eed2c7b6393ebc3fa28b7
dafaagasyadeliyardi <dafaagasyad@gmail.com>
2025-10-27 21:47:39 +0700

    latihan angka dan nilai
    latihan angka dan nilai

---
+++ latihan 1/nilai.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float tugas, tugas, nilaiakhir
+    cout <<"masukan nilai tugas: ";
+    cin <<tugas;
+    cout << "masukan nilai UTS: ";
+    cin >> uts;
+    cout << "masukan nilai UAS: ";
+    cin >> uas;
+
+    nilaiAkhir = (0.3 * tugas) + (0.3 * uts) + (0.3 * uas);
+
+    cout << "nilai Akhir =" << nilaiAkhir << endl;
+    if (nilaiAkhir>=60)
+
+    cout << "status lulus"<<endl;
+    else
+    {
+        cout <<
+    }
