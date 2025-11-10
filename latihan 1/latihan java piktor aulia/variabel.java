aca3c3d8101731ca7cc80f34e62ea6b9860a6d25
dafaagasyadeliyardi <dafaagasyad@gmail.com>
2025-11-10 19:24:55 +0700

    latihan java
    variabel java

---
+++ latihan 1/latihan java piktor aulia/variabel.java
@@ -0,0 +1,21 @@
+public class variabel {
+    public static void main (string[] args) {
+        // Deklarasi variabel dengan tipe data yang sesuai
+        String nama = " piktor aulia";
+        String tempattanggallahir = "Bogor, 18 maret 2005";
+        String jeniskelamin = "laki-laki";
+        String Alamatlengkap = "kp. pasir gaok, Bogor";
+        String nohp = "085714748408";
+        String penghasilanperbulan = 2500000.0;
+
+        System.out.println( "assalamuallaikum wr.wb");
+        System.out.println( "izinkan saya memperkenalkan diri");
+
+        System.out.println("Nama: " + nama);
+        System.out.println("tempat, tanggal lahir: " + tempattanggallahir);
+        System.out.println("jenis kelamin: " + jeniskelamin);
+        System.out.println("alamat lengkap: " + alamatlengkap);
+        System.out.println("no. hp: " + nohp);
+        System.out.println("penghasilan per bulan: rp " + penghasilanperbulan);
+    }
+} 
