839fe1ca491a9f1243c05c549abb46bd4103f5ac
dafaagasyadeliyardi <dafaagasyad@gmail.com>
2025-11-10 17:07:36 +0700

    latihan java
    variabel java

---
+++ latihan 1/latihan java/variable.java
@@ -0,0 +1,21 @@
+public class variable {
+    public static void main(String[] args) {
+        // Data diri
+        String namaLengkap = "dafa agasya deliyardi";
+        String tempatTanggalLahir = "Bogor, 10 febuari 2007";
+        String jenisKelamin = "Laki-laki";
+        String alamatLengkap = "Kp. girangsari rt3/rw8";
+        String noHp = "083879402287";
+        String penghasilanSebulan = "Rp5.000.000";
+
+        // Output variable
+        System.out.println("Assallamu'alaikum\n");
+        System.out.println("Izinkan saya memperkenalkan diri.\n");
+        System.out.println("Nama Lengkap            : " + namaLengkap);
+        System.out.println("Tempat & Tanggal Lahir  : " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin           : " + jenisKelamin);
+        System.out.println("Alamat Lengkap          : " + alamatLengkap);
+        System.out.println("No. HP                  : " + noHp);
+        System.out.println("Penghasilan Sebulan     : " + penghasilanSebulan);
+    }
+}
