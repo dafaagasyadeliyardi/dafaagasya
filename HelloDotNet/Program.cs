505eeb65a29a82137eb294e36ca33d1a6e972c7e
dafaagasyadeliyardi <dafaagasyad@gmail.com>
2026-01-27 21:07:40 +0700

    tugas net
    net tugas

---
+++ HelloDotNet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int umur = 18;
+        if (umur < 19)
+        {
+            Console.WriteLine("Umur belum Cukup");
+        }
+        else
+        {
+            Console.WriteLine("Umur Sudah Cukup");
+        }
+    }
+    
+}
